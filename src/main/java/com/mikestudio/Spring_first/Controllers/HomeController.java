a68f1dafe20bc44a446817b34c555154036d90fb
Pastamike29 <pastamike29@gmail.com>
2023-11-30 00:11:59 +0700

    edit SecureConfig CORS

--- src/main/java/com/mikestudio/Spring_first/Controllers/HomeController.java
+++ src/main/java/com/mikestudio/Spring_first/Controllers/HomeController.java
@@ -9,19 +9,16 @@ import org.springframework.web.servlet.function.EntityResponse;
 @RestController
 public class HomeController {
 
-    @CrossOrigin(origins = "http://localhost:2929")
     @GetMapping("/0")
     public String hello(){
         return  "Hello,World" ;
     }
 
-    @CrossOrigin(origins = "http://localhost:2929")
     @GetMapping("/0/home")
     public String homePage(){
         return "This is homepage";
     }
 
-    @CrossOrigin(origins = "http://localhost:2929")
     @GetMapping("/1/admin")
     public void adminPage(){
     }
--- src/main/java/com/mikestudio/Spring_first/config/SecurityConfig.java
+++ src/main/java/com/mikestudio/Spring_first/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.mikestudio.Spring_first.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.CoroutinesUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +15,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -49,6 +58,7 @@ public class SecurityConfig {
     @Order(2) //Ordering Request
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth ->{
                     auth.requestMatchers("/0/**").permitAll();
                     auth.requestMatchers("/error").permitAll();
@@ -58,6 +68,18 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:2929"));
+        configuration.setAllowedOrigins(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+
     }
 
 
